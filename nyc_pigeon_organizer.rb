efd0aa1c4fd96e1ec7e13bbcaccdbb7631ba7154
Camille Rheaume-Tremblay <camillerheaumet@hotmail.com>
2018-08-29 14:47:51 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,10 @@
+require pry 
+
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
 
   data.each do |info, options|
+    binding.pry
     options.each do |option, names|
       names.each do |name|
 
